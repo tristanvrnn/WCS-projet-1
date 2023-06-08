45aacb554645c6b586a94d5144ca1a913892b18a
tristan <tristan.vrn.code@gmail.com>
2023-06-08 12:29:18 +0200

    student.cs modification

--- student.cs
+++ student.cs
@@ -3,13 +3,26 @@ using System;
 public class Student
 {
     private int numericID;
-
     private string firstName;
-    private string surname;
-
+    private string lastName;
     private int birthdate;
+    private class Grade;
     
-    private double grades;
-    private string appreciation;
-    private double gradeAverage;
+    public double gradeAverage(double[] grades) 
+    {
+        double sum = 0;
+        foreach (double grade in grades)
+        {
+            sum += sum + grade;
+        }
+        double average = sum/grade.count;
+        return Math.Round(average,2);
+    }
+}
+
+public class Grade 
+{
+    private string subject;
+    private double[] grades;
+    private string[] appreciation;
 }
