6c5a24623be78338191ec3475453fde42ecbccce
tristan <tristan.vrn.code@gmail.com>
2023-06-08 11:07:19 +0200

    initialization project

---
+++ student.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Student
+{
+    private int numericID;
+
+    private string firstName;
+    private string surname;
+
+    private int birthdate;
+    
+    private double grades;
+    private string appreciation;
+    private double gradeAverage;
+}
